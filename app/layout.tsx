38b83f963a8af6efda8db76bb861c2f61a8359e7
Adam222372 <adamholecek22@gmail.com>
2025-04-14 02:23:13 +0200

    feat: add DotPattern component with glowing effect and update layout to use Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Funnel_Display } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
@@ -6,6 +7,10 @@ const funelDisplay = Funnel_Display({
     subsets: ['latin'],
     weight: ['400']
 })
+const InterFont = Inter({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
 
 export default async function RootLayout({
     children,
@@ -13,7 +18,7 @@ export default async function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html className={funelDisplay.className}>
+        <html className={InterFont.className}>
             <body>
                 {children}
             </body>
