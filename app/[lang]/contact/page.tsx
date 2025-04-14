5ae96db79ac4849b02dad806b3a5ee2194961eac
Adam222372 <adamholecek22@gmail.com>
2025-04-14 15:41:35 +0200

    fix: correct border styles for input fields and adjust section layout

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -2,23 +2,22 @@ import Image from "next/image";
 
 export default function Contact() {
     return (
-        <section className={"mt-25 "}>
-            <h2 className={"text-center text-2xl"}>Contact me</h2>
+        <section className={"mt-[100px] flex flex-col justify-center items-center"}>
             <div className={"flex justify-center flex-col items-center"}>
                   
                 <form action={"/contact"} method={"POST"} className={"flex flex-col my-8"}>
 
                     <div className="flex space-y-2 justify-between w-full ">
                     <input type={"email"} name={"email"} placeholder={"Email Address"}
-                           className={"border-b-3 border-gray-500 focus:border-white focus:outline-hidden my-8 w-55"}
+                           className={"border-b-2 border-gray-500 focus:border-white focus:outline-hidden my-8 w-55"}
                            required={true}/>
 
                     <input type={"text"} name={"subject"} placeholder={"Subject"}
-                           className={"border-b-3  border-gray-500 focus:border-white focus:outline-hidden my-8 w-55"}/>
+                           className={"border-b-2  border-gray-500 focus:border-white focus:outline-hidden my-8 w-55"}/>
 
                     </div>
                    
-                    <textarea name={"notes"} 
+                    <textarea name={"notes"} placeholder="Message" required={true}
                               className={" border-gray-500 border-3 focus:border-white focus:outline-hidden rounded-xl min-w-125 max-w-lg h-70 min-h-75 resize placeholder:ml-2 p-[1rem] "}></textarea>
 
                     <div className="flex justify-end items-center">
