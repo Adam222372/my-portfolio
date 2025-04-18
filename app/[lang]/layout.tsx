048204e5c37e81cc57c5131040b9ae42b0ba1e85
Adam222372 <adamholecek22@gmail.com>
2025-04-18 18:21:12 +0200

    made few changes to make functional build

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,18 +3,20 @@ import React from "react";
 import MenuBar from "../components/MenuBar";
 import { Inter } from "next/font/google";
 
+export const dynamic = "force-dynamic"; 
+
 const inter = Inter({
-    subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
   children,
   params,
 }: {
-  params: { lang: string };
+  params: Promise<{ lang: string }>; // Correct type for params
   children: React.ReactNode;
 }) {
-  const { lang } = await params;
+  const { lang } = await params; // Access lang directly from params
 
   return (
     <html lang={lang} className={inter.className}>
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,27 +1,38 @@
 import { DotPattern } from "../components/magicui/dot-pattern";
 import { cn } from "../../lib/utils";
 import React from "react";
+import { getDictionary } from "@/dictionaries/getDictionary";
 
-export const dynamic = "force-dynamic";
-
-export default async function Page({ params }: { params: { lang: string } }) {
-    //const { lang } = params;
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ lang: string }>; // Correctly type params
+}) {
+    const { lang } = await params; // Access lang directly from params
+    const dictionary = await getDictionary(lang); // Use params.lang
 
     return (
         <div>
             <div className="relative flex h-[500px] w-full flex-col items-center justify-center my-30">
-                <DotPattern className={cn(
-                    "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]"
-                )}>
-                </DotPattern>
+                <DotPattern
+                    className={cn(
+                        "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]"
+                    )}
+                ></DotPattern>
                 <div className="text-center backdrop-blur-xs rounded-lg p-4 bg-real-black/50 tracking-[-3px] w-[100%] md:w-[80%] lg:w-[60%] ">
-                    <h1 className="text-[96px]/25 flex flex-col font-semibold">Meaningful<span className="text-electric-blue tracking-[-5px] font-medium">Development</span></h1>
-                    <p className="m-4 py-4 tracking-[0.1em] text-xl ">Made by Adam Holeček</p>
+                    <h1 className="text-[96px]/25 flex flex-col font-semibold">
+                        {dictionary.heading.first}
+                        <span className="text-electric-blue tracking-[-5px] font-medium">
+                            {dictionary.heading.middle}
+                        </span>{" "}
+                        {dictionary.heading.last}
+                    </h1>
+                    <p className="m-4 py-4 tracking-[0.1em] text-xl ">
+                        {dictionary["heading-description"]}
+                    </p>
                 </div>
             </div>
-            <div className="bg-[linear-gradient(to_bottom,_#0a0a0a,_#171717_10%,_#252525_100%)] bg-cover bg-no-repeat bg-center h-[500px]">
-                   
-            </div>
+            <div className="bg-[linear-gradient(to_bottom,_#0a0a0a,_#171717_10%,_#252525_100%)] bg-cover bg-no-repeat bg-center h-[500px]"></div>
         </div>
-    )
+    );
 }
