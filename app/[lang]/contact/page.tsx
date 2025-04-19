4f8e46ecf61ede92bc3740051ea53bffeac480d2
Adam222372 <adamholecek22@gmail.com>
2025-04-19 22:36:57 +0200

    dictionary applied on contact directory

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -1,4 +1,9 @@
-export default function Contact() {
+import React from "react";
+import { getDictionary } from "@/dictionaries/getDictionary";
+
+export default async function Contact({params}: {params: Promise<{lang: string}>}) {
+    const { lang } = await params;
+    const dictionary = await getDictionary(lang);
     return (
         <div className={"mt-[100px] flex flex-col justify-center items-center"}>
             <div className={"flex justify-center flex-col items-center self-center "}>
@@ -6,20 +11,19 @@ export default function Contact() {
                 <form action={"/contact"} method={"POST"} className={"flex flex-col my-8"}>
 
                     <div className="flex space-y-2 justify-between w-full ">
-                    <input type={"email"} name={"email"} placeholder={"Email Address"}
-                           className={"border-b-2 border-luxury-gray focus:border-white focus:outline-hidden my-8 w-55"}
-                           required={true}/>
+                    <input type={"email"} name={"email"} placeholder={dictionary.contact.email} required={true}
+                           className={"border-b-2 border-luxury-gray focus:border-white focus:outline-hidden my-8 w-55"}/>
 
-                    <input type={"text"} name={"subject"} placeholder={"Subject"}
+                    <input type={"text"} name={"subject"} placeholder={dictionary.contact.subject} required={true}
                            className={"border-b-2  border-luxury-gray focus:border-white focus:outline-hidden my-8 w-55"}/>
 
                     </div>
                    
-                    <textarea name={"notes"} placeholder="Message" required={true}
+                    <textarea name={"notes"} placeholder={dictionary.contact.message} required={true}
                               className={" border-luxury-gray border-2 focus:border-white focus:outline-hidden rounded-xl min-w-125 max-w-lg h-70 min-h-75 resize placeholder:ml-2 p-[1rem] "}></textarea>
 
                     <div className="flex justify-end items-center">
-                    <button className={" bg-electric-blue text-white rounded-lg w-25 h-8 cursor-pointer m-4 hover:bg-electric-blue/70 transition font-medium"}>Submit</button>
+                    <button className={" bg-electric-blue text-white rounded-lg w-25 h-8 cursor-pointer m-4 hover:bg-electric-blue/70 transition font-medium"}>{dictionary.contact.send}</button>
                     </div>
                     
                 </form>
